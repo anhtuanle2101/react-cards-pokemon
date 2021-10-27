b31bf8693071d913a037e24628ded0a6035fa762
Tuan Anh Le <anhtuanle2101@gmail.com>
2021-10-27 16:16:06 -0400

    useAxios in PlayingCardList

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,7 @@
+import Axios from "axios";
 import React, {useState} from "react";
+import axios from "axios";
+import uuid from "uuid";
 
 const useFlip = (initVal) =>{
     const [state, setState] = useState(initVal);
@@ -9,4 +12,14 @@ const useFlip = (initVal) =>{
     return [state, toggleState];
 }
 
-export default useFlip;
+const useAxios = (url) =>{
+    const [state, setState] = useState([]);
+    const addItem = async ()=>{
+        const res = await axios.get(url);
+        setState(state => [...state, { ...res.data, id: uuid() }]);
+    }
+
+    return [state, addItem];
+}
+
+export {useFlip, useAxios};
